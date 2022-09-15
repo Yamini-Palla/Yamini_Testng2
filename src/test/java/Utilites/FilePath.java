02261cd85e1422345f9691b6df7824ce9941784e
Yamini-Palla <102537234+Yamini-Palla@users.noreply.github.com>
2022-09-15 14:32:00 +0530

    first commit

---
+++ src/test/java/Utilites/FilePath.java
@@ -0,0 +1,8 @@
+package Utilites;
+
+public class FilePath {
+
+	public static final String propertiesPath = "TestNg\\src\\test\\resources\\configFiles\\Config.properities";
+	public static final String XLPath = "TestNg\\ExcelFiles\\TestXL.xlsx";
+
+}
